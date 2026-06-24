d71a9a24060f45f85af7426270985467b2096967
Pratikpawarcr7 <pawarpratikcr7@gmail.com>
2026-06-24 02:37:49 -0400

    Add files via upload

--- Cpp_Programming/Program1.cpp
+++ Cpp_Programming/Program1.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+using namespace std;
 
+class Display
+{
+
+public:
+    int iNo1;
+
+    int Jay_Ganesh()
+    {
+        cout << "Jay Ganesh..." << "\n";
+    }
+};
+int main()
+{
+    Display dobj;
+    dobj.Jay_Ganesh();
+
+    return 0;
+}
